d6fee8d956414902c87efb373bf3d27ea8c37142
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-08-07 21:02:19 +0200

    3. Alternative Props Syntax

--- 3_react_essentials/src/App.jsx
+++ 3_react_essentials/src/App.jsx
@@ -1,6 +1,5 @@
-import componentsImg from './assets/components.png';
 import reactImg from './assets/react-core-concepts.png';
-
+import { CORE_CONCEPTS } from './data.js';
 const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 const genRandomInt = (max) => Math.floor(Math.random() * (max + 1));
 
@@ -18,12 +17,12 @@ const Header = (_) => {
     </header>
   );
 };
-const CoreConcept = (props) => {
+const CoreConcept = ({image, title, description}) => {
   return (
     <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
     </li>
   );
 };
@@ -36,11 +35,13 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept
-              title='Components'
-              description='The core UI building block'
-              image={componentsImg}
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
             />
-         
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
       </main>
