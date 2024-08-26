11fcb4622ea5eebb85ef8045b81e7fc9b4f1dd11
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-08-26 21:05:07 +0200

    9. Handling Project Deletion

--- 9_projectManagement/src/App.jsx
+++ 9_projectManagement/src/App.jsx
@@ -34,10 +34,22 @@ function App() {
       selectedProjectId: id,
     }));
 
-  const selectedProject = projectState.projects.find(project => project.id === projectState.selectedProjectId)
+  const handleDeleteProject = () =>
+    setProjectState((prevState) => ({
+      ...prevState,
+      selectedProjectId: undefined,
+      projects: prevState.projects.filter(
+        (project) => project.id !== prevState.selectedProjectId
+      ),
+    }));
+  const selectedProject = projectState.projects.find(
+    (project) => project.id === projectState.selectedProjectId
+  );
 
-  let content = <SelectedProject project={selectedProject} />;
-  
+  let content = (
+    <SelectedProject project={selectedProject} onDelete={handleDeleteProject} />
+  );
+
   if (projectState.selectedProjectId === null) {
     content = (
       <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
