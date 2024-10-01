2ae0632e9acb8c9e8249f5bc801214909244c8b9
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-10-01 10:42:24 +0200

    19. Creating a Redux Store for React

---
+++ 19_redux/src/store/index.js
@@ -0,0 +1,20 @@
+import { createStore } from 'redux';
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === 'increment') {
+    return {
+      counter: state.counter++,
+    };
+  }
+
+  if (action.type === 'decrement') {
+    return {
+      counter: state.counter--,
+    };
+  }
+  return state;
+};
+
+const store = createStore(counterReducer);
+
+export default store;
