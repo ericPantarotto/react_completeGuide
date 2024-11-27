6e9954f41a431dc32df873889146a96a8d2ceb37
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-11-27 14:46:53 +0100

    27. Grouping Compound Components

--- 27_react_patterns/src/components/Accordion.jsx
+++ 27_react_patterns/src/components/Accordion.jsx
@@ -1,13 +1,23 @@
 import PropTypes from 'prop-types';
 import { createContext, useState } from 'react';
+import AccordionItem from './AccordionItem';
 
 const AccordionContext = createContext();
 
 const Accordion = ({ children, className }) => {
   const [openItemId, setOpenItemId] = useState();
-  const openItem = (id) => setOpenItemId(id);
-  const closeItem = () => setOpenItemId(null);
-  const contextValue = { openItemId: openItemId, openItem, closeItem };
+
+  // const openItem = (id) => setOpenItemId(id);
+  // const closeItem = () => setOpenItemId(null);
+  // const contextValue = { openItemId: openItemId, openItem, closeItem };
+
+  const toggleItem = (id) =>
+    setOpenItemId((prevId) => (prevId === id ? null : id));
+
+  const contextValue = {
+    openItemId: openItemId,
+    toggleItem,
+  };
 
   return (
     <AccordionContext.Provider value={contextValue}>
@@ -20,5 +30,7 @@ Accordion.propTypes = {
   className: PropTypes.string.isRequired,
 };
 
+Accordion.Item = AccordionItem;
+
 export default Accordion;
-export {AccordionContext}
+export { AccordionContext };
--- 27_react_patterns/src/components/AccordionItem.jsx
+++ 27_react_patterns/src/components/AccordionItem.jsx
@@ -2,20 +2,22 @@ import PropTypes from 'prop-types';
 import { useAccordionContext } from './UseAccordionContext';
 
 const AccordionItem = ({ id, children, title, className }) => {
-  const { openItemId, openItem, closeItem } = useAccordionContext();
+  // const { openItemId, openItem, closeItem } = useAccordionContext();
+  const { openItemId, toggleItem } = useAccordionContext();
 
   const isOpen = openItemId === id;
 
-  const handleClick = () => {
-    if (isOpen) {
-      closeItem();
-    } else {
-      openItem(id);
-    }
-  };
+  // const handleClick = () => {
+  //   if (isOpen) {
+  //     closeItem();
+  //   } else {
+  //     openItem(id);
+  //   }
+  // };
+
   return (
     <li className={className}>
-      <h3 onClick={handleClick}>{title}</h3>
+      <h3 onClick={() => toggleItem(id)}>{title}</h3>
       <div
         className={
           isOpen ? 'accordion-item-content open' : 'accordion-item-content'
