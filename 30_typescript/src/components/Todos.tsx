5816a0595c76be9edb21e4bd9403314f72940ba1
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-12-07 23:17:44 +0100

    30. Adding Styling

--- 30_typescript/src/components/Todos.tsx
+++ 30_typescript/src/components/Todos.tsx
@@ -1,11 +1,12 @@
 import { FC } from 'react';
 import Todo from '../models/todo';
 import TodoItem from './TodoItem';
+import classes from './Todos.module.css';
 
 // const Todos: FC<{ items: string[] }> = (props) => {
 const Todos: FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={classes.todos}>
       {props.items.map((item) => (
         // <li key={item}>{item}</li>
         // <li key={item.id}>{item.text}</li>
