940b7906e7c98c91d94822f44c5bd9323758f489
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-12-08 12:03:23 +0100

    31. CSS Styling & CSS Modules

--- 31_summary/src/components/Post.jsx
+++ 31_summary/src/components/Post.jsx
@@ -1,10 +1,11 @@
 import PropTypes from 'prop-types';
+import classes from './Post.module.css';
 
 const Post = (props) => {
   return (
-    <div>
-      <p>{props.author}</p>
-      <p>{props.body}</p>
+    <div className={classes.post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.text}>{props.body}</p>
     </div>
   );
 };
