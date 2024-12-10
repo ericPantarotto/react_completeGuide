f838ab0545c38177bc1bd6b8178cfbf5e7ef66a1
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-12-11 00:03:34 +0100

    31. Handling Form Submission

--- 31_summary/src/components/PostsList.jsx
+++ 31_summary/src/components/PostsList.jsx
@@ -1,59 +1,40 @@
 import PropTypes, { bool } from 'prop-types';
-import { useState } from 'react';
 import Modal from './Modal';
 import NewPost from './NewPost';
 import Post from './Post';
 
 import classes from './PostsList.module.css';
 
-const PostList = ({ isPosting, onStopPosting }) => {
-  const [enteredBody, setEnteredBody] = useState('');
-  const [enteredAuthor, setEnteredAuthor] = useState('');
+const PostList = ({ isPosting, onStopPosting }) => (
+  <>
+    {isPosting && (
+      <Modal onClose={onStopPosting}>
+        <NewPost onCancel={onStopPosting} />
+      </Modal>
+    )}
 
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
-  // let modalContent;
-
-  // if (modalIsVisible) {
-  //   modalContent = (
-  // <Modal onClose={hideModalHandler}>
-  //   <NewPost
-  //     onBodyChange={bodyChangeHandler}
-  //     onAuthorChange={authorChangeHandler}
-  //   />
-  // </Modal>
-  //   );
-  // }
-
-  return (
-    <>
-      {isPosting && (
-        <Modal onClose={onStopPosting}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-            onCancel={onStopPosting}
-          />
-        </Modal>
-      )}
-
-      <ul className={classes.posts}>
-        <Post author={enteredBody} body={enteredAuthor} />
-        <Post author='Manuel' body='Check out the full course!' />
-      </ul>
-    </>
-  );
-};
+    <ul className={classes.posts}>
+      <Post author='Manuel' body='Check out the full course!' />
+    </ul>
+  </>
+);
 
 PostList.propTypes = {
   onStopPosting: PropTypes.func,
-  isPosting: bool
+  isPosting: bool,
 };
 
 export default PostList;
+
+// let modalContent;
+
+// if (modalIsVisible) {
+//   modalContent = (
+// <Modal onClose={hideModalHandler}>
+//   <NewPost
+//     onBodyChange={bodyChangeHandler}
+//     onAuthorChange={authorChangeHandler}
+//   />
+// </Modal>
+//   );
+// }
