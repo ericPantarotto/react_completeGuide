4cc6423c6a3ca9abecbb67378742ddb346cc6c9d
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-12-18 09:11:03 +0100

    31. Handle Loading State

--- 31_summary_be/app.js
+++ 31_summary_be/app.js
@@ -18,7 +18,8 @@ app.use((req, res, next) => {
 
 app.get('/posts', async (req, res) => {
   const storedPosts = await getStoredPosts();
-  // await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
+  // HACK: to simulate some delay
+  await new Promise((resolve, reject) => setTimeout(() => resolve(), 1500));
   res.json({ posts: storedPosts });
 });
 
