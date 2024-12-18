bd3313a6857294113a3960c03d220daaf1b41a19
Eric Carlier Pantarotto <eric.carlier@gmail.com>
2024-12-18 21:36:33 +0100

    17b. Managing Form-dependent State with useActionState()

--- 17_formactions/src/components/Signup.jsx
+++ 17_formactions/src/components/Signup.jsx
@@ -1,12 +1,13 @@
+import { useActionState } from 'react';
 import {
-  isEmail,
-  isNotEmpty,
-  isEqualToOtherValue,
   hasMinLength,
+  isEmail,
+  isEqualToOtherValue,
+  isNotEmpty,
 } from '../util/validation';
 
 export default function Signup() {
-  function signupAction(formData) {
+  function signupAction(_, formData) { //NOTE: _ = previous form state
     const email = formData.get('email');
     const password = formData.get('password');
     const confirmPassword = formData.get('confirm-password');
@@ -45,10 +46,20 @@ export default function Signup() {
     if (acquisitionChannel.length === 0) {
       errors.push('Please select at least one acquisition channel.');
     }
+
+    if (errors.length > 0) {
+      return { errors };
+    }
+
+    return { errors: null };
   }
 
+  const [formState, formAction] = useActionState(signupAction, {
+    errors: null,
+  });
+
   return (
-    <form action={signupAction}>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -132,6 +143,14 @@ export default function Signup() {
           agree to the terms and conditions
         </label>
       </div>
+
+      {formState.errors && (
+        <ul className='error'>
+          {formState.errors.map((error) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
 
       <p className='form-actions'>
         <button type='reset' className='button button-flat'>
